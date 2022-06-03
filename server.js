ccdc6d7e57c36176358367c6815e4f28f0180584
cavicphi <cavicphi@students.zhaw.ch>
2022-06-03 21:26:12 +0200

    2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Philip :-)');
+  res.send('Hello Philip, new try:-)');
 });
 
 app.listen(PORT, HOST);
